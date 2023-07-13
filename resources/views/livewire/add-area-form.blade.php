1ddd48bdd5d3a6a3229ff0e4ab8472bd319c1308
codingwithhassan <codingwithme.hassan@gmail.com>
2023-07-13 23:04:49 +0500

    correct input fields and add asterisk to required fields

--- resources/views/livewire/add-area-form.blade.php
+++ resources/views/livewire/add-area-form.blade.php
@@ -14,15 +14,15 @@
     </div>
     <div class="col-12">
         <form>
-            <div class="mb-3">
+            <div class="form-group required mb-3">
                 <label for="name" class="form-label">Name</label>
-                <input type="email" class="form-control @error('name') is-invalid @enderror" id="name" wire:model.debounce.500ms="name">
+                <input placeholder="Enter Area Name" type="email" class="form-control @error('name') is-invalid @enderror" id="name" wire:model.debounce.500ms="name">
                 @error('name')
                 <span class="invalid-feedback">{{ $message }}</span>
                 @enderror
             </div>
-            <div class="mb-3">
-                <label for="category" class="form-label">Select Category</label>
+            <div class="form-group required mb-3">
+                <label for="category" class="form-label">Category</label>
                 <select class="form-select @error('categoryId') is-invalid @enderror" id="category" wire:model="categoryId">
                     <option selected>Select Category</option>
                     @foreach($categories as $id => $category)
@@ -33,21 +33,21 @@
                 <span class="invalid-feedback">{{ $message }}</span>
                 @enderror
             </div>
-            <div class="mb-3">
+            <div class="form-group required mb-3">
                 <label for="start_date" class="form-label">Start Date</label>
                 <input type="date" class="form-control @error('startDate') is-invalid @enderror" id="start_date" wire:model="startDate">
                 @error('startDate')
                 <span class="invalid-feedback">{{ $message }} </span>
                 @enderror
             </div>
-            <div class="mb-3">
+            <div class="form-group required mb-3">
                 <label for="end_date" class="form-label">End Date</label>
                 <input type="date" class="form-control @error('endDate') is-invalid @enderror" id="end_date" wire:model="endDate">
                 @error('endDate')
                 <span class="invalid-feedback"> {{ $message }} </span>
                 @enderror
             </div>
-            <div class="mb-3">
+            <div class="form-group required mb-3">
                 <label for="owner" class="form-label">Owner</label>
                 <select class="form-select @error('ownerId') is-invalid @enderror" id="owner" wire:model="ownerId">
                     <option selected>Select Owner</option>
@@ -60,9 +60,8 @@
                 @enderror
             </div>
             <div class="mb-3">
-                <label for="file" class="form-label">End Date</label>
+                <label for="file" class="form-label">GeoJson File <span style="color: #b2b2b2">(GeoJson file with polygon)</span></label>
                 <input type="file" onchange="handleFileSelect(event)" class="form-control @error('file') is-invalid @enderror" id="file" wire:model="file">
-                <span class="valie-feedback">GeoJson file with polygon</span>
                 @error('file')
                 <span class="invalid-feedback"> {{ $message }} </span>
                 @enderror
